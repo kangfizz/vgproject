a1f2fb030e67d2996a043e03c92853f50657e6a6
kangfizz <kangfizz@ap-mic.com>
2019-05-31 10:20:47 +0800

    feat(api.views): vg_today_card parse

--- api/views.py
+++ api/views.py
@@ -12,13 +12,29 @@ class Api:
     def __init__(self, _type):
         if _type == 'WS':
             self.url = "https://ws-tcg.com"
+        elif _type == 'VG':
+            self.url = "https://cf-vanguard.com"
+
+    @staticmethod
+    def saveimage(find_list, _route, front_route=""):
+        """儲存Image to 本地"""
+        url_list = [f"{front_route}{x.get('src')}" for x in find_list]
+        route = os.path.join(settings.BASE_DIR, _route)
+
+        return urllib.request.urlretrieve(url_list[0], route)
 
     def get_ws_today_card(self):
         """取得每日一卡(WS)"""
         html = requests.get(f"{self.url}/ws_today_p/")
         soup = BeautifulSoup(html.text, 'html.parser')
         _list = soup.findAll('img', 'aligncenter')
-        url_list = [f"{self.url}{x.get('src')}" for x in _list]
-        route = os.path.join(settings.BASE_DIR, 'static/upload/todays_card.png')
 
-        return urllib.request.urlretrieve(url_list[0], route)
+        return self.saveimage(_list, 'static/upload/todays_card_ws.png', self.url)
+
+    def get_vg_today_card(self):
+        """取得每日一卡(VG)"""
+        html = requests.get(f"{self.url}/todays-card/")
+        soup = BeautifulSoup(html.text, 'html.parser')
+        _list = soup.findAll('img', alt='【カードファイト!! ヴァンガード】今日のカード')
+
+        return self.saveimage(_list, 'static/upload/todays_card_vg.png')
--- period.py
+++ period.py
@@ -12,6 +12,9 @@ from api.views import Api
 
 NOW = datetime.now() + timedelta(hours=8)
 print(NOW)
+print("===== vg todays card catch =====")
+if datetime.strftime(NOW, "%H:%M") == "09:31":
+    Api("VG").get_vg_today_card()
 print("===== ws todays card catch =====")
 if datetime.strftime(NOW, "%H:%M") == "09:32":
     Api("WS").get_ws_today_card()

e3f8a5f241f7cf7c79de592bdd898c39123f92bd
kangfizz <kangfizz@ap-mic.com>
2019-05-31 15:34:59 +0800

    fix(static): reset static path

--- vgsite/urls.py
+++ vgsite/urls.py
@@ -1,17 +1,16 @@
 """vgsite URL Configuration
 
 """
+import django
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from offwebsite import views as oviews
 
 urlpatterns = [
     url(r'^$', oviews.fullpage),
     url(r'^admin/', admin.site.urls),
     url(r'^vgsite/', include('offwebsite.urls')),
+    url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
